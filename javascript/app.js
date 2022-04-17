b376be01d78f0f272c326d27462b821df04eb872
sindhu-149 <sindhukanakarao149@gmail.com>
2022-04-17 11:02:48 +0530

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,104 @@
+var count=0;
+function play() {
+  if(count == 0){
+        count = 1;
+        var audio = document.getElementById("audio");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count = 0;
+        audio.pause();
+        }
+}
+var count1=0;
+function play1() {
+  if(count1 == 0){
+        count1 = 1;
+        var audio = document.getElementById("audio-1");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count1 = 0;
+        audio.pause();
+        }
+}
+var count2=0;
+function play2() {
+  if(count2 == 0){
+        count2 = 1;
+        var audio = document.getElementById("audio-2");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count2 = 0;
+        audio.pause();
+        }
+}
+var count3=0;
+function play3() {
+  if(count3 == 0){
+        count3 = 1;
+        var audio = document.getElementById("audio-3");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count3 = 0;
+        audio.pause();
+        }
+}
+var count4=0;
+function play4() {
+  if(count4 == 0){
+        count4 = 1;
+        var audio = document.getElementById("audio-4");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count4 = 0;
+        audio.pause();
+        }
+}
+var count5=0;
+function play5() {
+  if(count5 == 0){
+        count5 = 1;
+        var audio = document.getElementById("audio-5");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count5 = 0;
+        audio.pause();
+        }
+}
+var count6=0;
+function play6() {
+  if(count6 == 0){
+        count6 = 1;
+        var audio = document.getElementById("audio-6");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count6 = 0;
+        audio.pause();
+        }
+}
+var count7=0;
+function play7() {
+  if(count7 == 0){
+        count7 = 1;
+        var audio = document.getElementById("audio-7");
+          audio.play();
+        audio.loop=True;
+    }
+    else{
+        count7  = 0;
+        audio.pause();
+        }
+}
