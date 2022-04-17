03ed1a6567077bac0ee0631aec0440f50bfd3442
sindhu-149 <sindhukanakarao149@gmail.com>
2022-04-17 18:39:57 +0530

    almost ready

--- javascript/app.js
+++ javascript/app.js
@@ -1,104 +1,113 @@
-var count1=0;
-function playPause1(){
-    if(count1 == 0){
-        count1 = 1
-        audio1.play();
+var count1 = 0;
 
-    }else{
-        count1 = 0;
-        audio1.pause();
-
-    }
+function playPause1() {
+  if (count1 == 0) {
+    count1 = 1
+    audio1.play();
+    document.getElementById('playPauseBTN1').setAttribute("class", "style1");
+    // playPauseBTN1.innerHTML = "⏸";
+  } else {
+    count1 = 0;
+    audio1.pause();
+    // playPauseBTN1.innerHTML = "▶️";
+  }
 }
 
 
-var count2=0;
-function playPause2(){
-    if(count2 == 0){
-        count2 = 1;
-        audio2.play();
+var count2 = 0;
 
-    }else{
-        count2 = 0;
-        audio2.pause();
-
-    }
+function playPause2() {
+  if (count2 == 0) {
+    count2 = 1;
+    audio2.play();
+    document.getElementById('playPauseBTN2').setAttribute("class", "style1");
+  } else {
+    count2 = 0;
+    audio2.pause();
+    // playPauseBTN2.innerHTML = "▶️";
+  }
 }
 
-var count3=0;
-function playPause3(){
-    if(count3 == 0){
-        count3 = 1;
-        audio3.play();
+var count3 = 0;
 
-    }else{
-        count3 = 0;
-        audio3.pause();
-
-    }
+function playPause3() {
+  if (count3 == 0) {
+    count3 = 1;
+    audio3.play();
+    document.getElementById('playPauseBTN3').setAttribute("class", "style1");
+  } else {
+    count3 = 0;
+    audio3.pause();
+    // playPauseBTN3.innerHTML = "▶️";
+  }
 }
 
-var count4=0;
-function playPause4(){
-    if(count4 == 0){
-        count4 = 1;
-        audio4.play();
+var count4 = 0;
 
-    }else{
-        count4 = 0;
-        audio4.pause();
-
-    }
+function playPause4() {
+  if (count4 == 0) {
+    count4 = 1;
+    audio4.play();
+    document.getElementById('playPauseBTN4').setAttribute("class", "style1");
+  } else {
+    count4 = 0;
+    audio4.pause();
+    // playPauseBTN4.innerHTML = "▶️";
+  }
 }
 
-var count5=0;
-function playPause5(){
-    if(count5 == 0){
-        count5 = 1;
-        audio5.play();
+var count5 = 0;
 
-    }else{
-        count5 = 0;
-        audio5.pause();
-
-    }
+function playPause5() {
+  if (count5 == 0) {
+    count5 = 1;
+    audio5.play();
+    document.getElementById('playPauseBTN5').setAttribute("class", "style1");
+  } else {
+    count5 = 0;
+    audio5.pause();
+    // playPauseBTN5.innerHTML = "▶️";
+  }
 }
 
-var count6=0;
-function playPause6(){
-    if(count6 == 0){
-        count6 = 1;
-        audio6.play();
+var count6 = 0;
 
-    }else{
-        count6 = 0;
-        audio6.pause();
-
-    }
+function playPause6() {
+  if (count6 == 0) {
+    count6 = 1;
+    audio6.play();
+    document.getElementById('playPauseBTN6').setAttribute("class", "style1");
+  } else {
+    count6 = 0;
+    audio6.pause();
+    // playPauseBTN6.innerHTML = "▶️";
+  }
 }
 
-var count7=0;
-function playPause7(){
-    if(count7 == 0){
-        count7 = 1;
-        audio7.play();
+var count7 = 0;
 
-    }else{
-        count7 = 0;
-        audio7.pause();
-
-    }
+function playPause7() {
+  if (count7 == 0) {
+    count7 = 1;
+    audio7.play();
+    document.getElementById('playPauseBTN7').setAttribute("class", "style1");
+  } else {
+    count7 = 0;
+    audio7.pause();
+    // playPauseBTN7.innerHTML = "▶️";
+  }
 }
 
-var count8=0;
-function playPause8(){
-    if(count8 == 0){
-        count8 = 1;
-        audio8.play();
+var count8 = 0;
 
-    }else{
-        count8 = 0;
-        audio8.pause();
-    
-    }
+function playPause8() {
+  if (count8 == 0) {
+    count8 = 1;
+    audio8.play();
+    document.getElementById('playPauseBTN8').setAttribute("class", "style1");
+  } else {
+    count8 = 0;
+    audio8.pause();
+    // playPauseBTN8.innerHTML = "▶️";
+  }
 }
